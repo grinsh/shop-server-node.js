cbaaca9e9c26686e87003fdedde7fe04e89b5e50
grinsh <105662005+grinsh@users.noreply.github.com>
2022-05-22 00:07:04 +0300

    adding server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const  express =  require( 'express');
+const cors = require( 'cors');
+
+const PORT = 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(PORT, () => console.log(`Server is up at ${PORT}`));
